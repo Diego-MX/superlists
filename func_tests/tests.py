d2615db6540177d0474e47a295050901faf6ac07
Diego Villamil <diego_villamil@outlook.com>
2015-12-07 19:38:44 -0600

    make func_tests an app, use LiveServerTestCase

--- func_tests/tests.py
+++ func_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import unittest, time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):  
         self.browser = webdriver.Firefox()
