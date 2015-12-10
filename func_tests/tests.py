2f4cca6c347fcc3bb559d4c46a36a973d0cce562
Diego Villamil <diego_villamil@outlook.com>
2015-12-09 20:17:59 -0600

    Update functional test to consider changes in the model.

--- func_tests/tests.py
+++ func_tests/tests.py
@@ -20,8 +20,9 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
-
+        # self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
+        
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -37,8 +38,10 @@ class NewVisitorTest(LiveServerTestCase):
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page lists "1: Buy peacock feathers" as an item in a to-do list
+        # When she hits enter, she is taken to a new URL, and now the page lists "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+') #1
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
