0d5536444817f7db9c3327315ba3222539aab41a
Diego Villamil <diego_villamil@outlook.com>
2016-07-04 20:30:52 -0500

    functional test for javascript handling

--- lists/forms.py
+++ lists/forms.py
@@ -37,11 +37,5 @@ class ExistingListItemForm(ItemForm):
             err.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(err)
 
-
-# class ItemForm(forms.Form):
-#     item_text = forms.CharField(
-#         widget=forms.fields.TextInput(attrs={
-#             'placeholder':  'Enter a to-do item',
-#             'class':        'form-control input-lg',
-#         }),
-#     )
+    def save(self):
+        return forms.models.ModelForm.save(self)
